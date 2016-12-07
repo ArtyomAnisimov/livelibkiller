eaad77f9768542a5d9e783c9c72dc192d8bc0ae8
pavel <ordinarypavel@gmail.com>
2016-12-07 17:23:21 +0700

    Описана модель UsersBook

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,11 +1,20 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from apps.books.models import Book
 from django.contrib.auth.models import User
 
 
 class UsersBook(models.Model):
-    pass
-
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(verbose_name=u'Название',max_length=200)
+	comment = models.CharField(verbose_name=u'Комментарий',max_length=200)
+	user = models.ForeignKey('auth.User',verbose_name=u'Пользователь')
+	fav = models.BooleanField()
+	read = models.BooleanField()
+	wish = models.BooleanField()
+	def __str__(self):
+        return str(self.name)
 
 class UserProfile(models.Model):
     pass
