7d68d4a830b1fe77dd3857804fd3ffa3c55455c0
artem <artem_anisimov_1997@mail.ru>
2016-12-07 21:15:39 +0700

    method

--- apps/home/views.py
+++ apps/home/views.py
@@ -4,6 +4,7 @@ from app.requests.forms import RequestForm
 from app.requests.models import Request
 from django.http import HttpResponseRedirect
 from django.views.generic import FormView
+from myapp.forms import ContactForm
 #Кажется, выше импорты нужны
 from django.shortcuts import render
 from django.views.generic import TemplateView, FormView
@@ -13,19 +14,10 @@ class HomeView(TemplateView):
 
 class LoginFormView(FormView):
     template_name = "/home/login.html"
-
-    form_class = AuthenticationForm
-
-    def get_success_url(self):
-        next = self.request.GET.get('next')
-        if next:
-            return next
-        return '/home/'
+    login_class = ContactForm
+    success_url = '/'
 
     def form_valid(self, form):
-        self.user = form.get_user()
-        login(self.request, self.user)
-        if self.request.user.is_authenticated:
-            return super(LoginFormView, self).form_valid(form)
-        return super(LoginFormView, self).form_invalid(form)
+        return super(ContactView, self).form_valid(form)
+    
 
