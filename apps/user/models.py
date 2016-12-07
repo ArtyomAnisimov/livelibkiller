d5f35d7ecadf71833201581c7f9d51689daf952d
Den125 <sasha_isaikin@mail.ru>
2016-12-07 20:13:46 +0700

    update

--- apps/user/models.py
+++ apps/user/models.py
@@ -17,4 +17,10 @@ class UsersBook(models.Model):
         return str(self.name)
 
 class UserProfile(models.Model):
-    user_profile= "/apps/templates/user/index.html
+    user = models.OneToOneField(User, verbose_name='Пользователь')
+    nickname = models.CharField(verbose_name='Имя', max_length=100)
+    date_joined = models.DateField(verbose_name='Дата регистрации')
+    email = models.EmailField(verbose_name='Email', max_length=200)
+    rating = models.IntegerField(verbose_name='Рейтинг')
+    status = models.CharField(verbose_name='Статус', max_length=160)
+    about = models.TextField(verbose_name='О себе', max_length=160)
