58d1b31a11b51ea96de9063b1bcfb79e8b04bbf3
Илья <krylovilya1@gmail.com>
2016-12-13 16:06:40 +0700

    #26

--- apps/home/views.py
+++ apps/home/views.py
@@ -9,7 +9,8 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['User'] = User
+        user = User
+        context['User'] = user
         return context
 
 
